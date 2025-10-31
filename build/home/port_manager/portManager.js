8e564b90bbdfb9d92a12517177851000ce04251b
Jake Jester <jake.jester@gmail.com>
2025-10-31 16:35:24 -0400

    - Adding ignore to build results. - Got rid of mycustomui.tsx, was a internet example for debugging ns concurrent problem.

--- build/home/port_manager/portManager.js
+++
@@ -1,8 +0,0 @@
-// servers/home/port_manager/portManager.ts
-var PORT_LIST = /* @__PURE__ */ ((PORT_LIST2) => {
-  PORT_LIST2[PORT_LIST2["SERVER_LIST"] = 1] = "SERVER_LIST";
-  return PORT_LIST2;
-})(PORT_LIST || {});
-export {
-  PORT_LIST
-};
